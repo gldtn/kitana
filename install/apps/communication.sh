728cb62e978470cebb08832b80ece5254f401df6
Glaudston Soares <g@glaudston.com>
2026-05-25 19:45:15 -0400

    Make Zoom install non-blocking

--- install/apps/communication.sh
+++ install/apps/communication.sh
@@ -2,11 +2,17 @@
 
 echo "Installing communication apps..."
 
-PACKAGES=(
+REQUIRED_PACKAGES=(
   vesktop-bin
-  zoom
 )
 
-for pkg in "${PACKAGES[@]}"; do
+for pkg in "${REQUIRED_PACKAGES[@]}"; do
   yay -S --noconfirm --needed "$pkg"
 done
+
+if ! yay -S --noconfirm --needed zoom; then
+  echo "Zoom install failed. Retrying with a clean AUR build..."
+  if ! yay -S --noconfirm --needed --answerclean All zoom; then
+    echo "WARNING: Zoom failed to install and needs manual intervention. Continuing."
+  fi
+fi
