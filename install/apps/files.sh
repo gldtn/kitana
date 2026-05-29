1793ae30e96d44cd964fc398fcac2cd65c41f4ed
Glaudston Soares <g@glaudston.com>
2026-05-29 18:09:25 -0400

    Make Dropbox packages optional

--- install/apps/files.sh
+++ install/apps/files.sh
@@ -3,17 +3,29 @@
 echo "Installing file-manager and integration apps..."
 
 PACKAGES=(
-    dropbox
-    dropbox-cli
     ffmpegthumbnailer
     libappindicator-gtk3
     nautilus
-    nautilus-dropbox
     nautilus-image-converter
     python-gpgme
     sushi
 )
 
+OPTIONAL_PACKAGES=(
+    dropbox
+    dropbox-cli
+    nautilus-dropbox
+)
+
 for pkg in "${PACKAGES[@]}"; do
     yay -S --noconfirm --needed "$pkg"
+done
+
+for pkg in "${OPTIONAL_PACKAGES[@]}"; do
+    if ! yay -S --noconfirm --needed "$pkg"; then
+        echo "$pkg install failed. Retrying with a clean AUR build..."
+        if ! yay -S --noconfirm --needed --answerclean All "$pkg"; then
+            echo "WARNING: $pkg failed to install and needs manual intervention. Continuing."
+        fi
+    fi
 done
