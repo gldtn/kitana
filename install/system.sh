3bc2375b07cc8fc764f076dfaa0a2fe73e7b2d87
gldtn <g@glaudston.com>
2026-06-05 17:20:19 -0400

    Make system install standalone

--- install/system.sh
+++ install/system.sh
@@ -2,6 +2,16 @@
 
 # system.sh: Core system and hardware package stages.
 
+set -e
+
+export KITANA_DIR="${KITANA_DIR:-$HOME/.local/share/kitana}"
+export KITANA_INSTALL="${KITANA_INSTALL:-$KITANA_DIR/install}"
+
+# shellcheck source=lib/install.sh
+source "$KITANA_INSTALL/lib/install.sh"
+kitana_init_paths
+kitana_init_install_log
+
 echo "Installing system packages..."
 
 source_script "system/base.sh"
