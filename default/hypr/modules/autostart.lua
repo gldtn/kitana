016973ab49aa4a65edb7405a22c591b3237922b8
gldtn <g@glaudston.com>
2026-06-08 17:28:19 -0400

    Stabilize Hyprland workspace sync

--- default/hypr/modules/autostart.lua
+++ default/hypr/modules/autostart.lua
@@ -4,11 +4,15 @@
 
 -- See https://wiki.hypr.land/Configuring/Basics/Autostart/
 
+local function sync_workspaces()
+  hl.exec_cmd("${KITANA_DIR:-$HOME/.local/share/kitana}/bin/kitana-workspaces")
+end
+
 -- Example:
 hl.on("hyprland.start", function()
   hl.exec_cmd("systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP XDG_SESSION_TYPE XDG_SESSION_DESKTOP")
   hl.exec_cmd("dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP XDG_SESSION_TYPE XDG_SESSION_DESKTOP")
-  hl.exec_cmd("${KITANA_DIR:-$HOME/.local/share/kitana}/bin/kitana-workspaces")
+  sync_workspaces()
   hl.exec_cmd("${KITANA_DIR:-$HOME/.local/share/kitana}/bin/kitana-wallpaper --restore")
   hl.exec_cmd("${KITANA_DIR:-$HOME/.local/share/kitana}/bin/kitana-wallpaper-watch")
   hl.exec_cmd("systemctl --user start hyprpolkitagent")
@@ -16,3 +20,5 @@ hl.on("hyprland.start", function()
     -- hl.exec_cmd("systemctl --user start hypridle.service")
   hl.exec_cmd("hypridle")
 end)
+
+hl.on("config.reloaded", sync_workspaces)
